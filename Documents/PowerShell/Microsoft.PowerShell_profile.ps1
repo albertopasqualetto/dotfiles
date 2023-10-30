6a3d9e7a2424a55fe68166f157444e1c90daf608
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2023-10-30 17:29:46 +0100

    Add Documents/PowerShell/Microsoft.PowerShell_profile.ps1

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,38 @@
+### ALIASES ###
+function SSHOracleVM {
+	ssh ubuntu@129.152.19.184
+}
+function PyActivate {
+	.\venv\Scripts\activate.ps1
+}
+function wgu {
+	winget upgrade $args
+}
+function notepadpp {
+	start notepad++ $args
+}
+function npp {
+	[String]$arg = $args
+	notepadpp $arg
+}
+function grep {
+	Select-String $args
+}
+function which {
+	where $args
+}
+
+
+### STARSHIP ###
+function Invoke-Starship-TransientFunction {
+  &starship module character
+}
+
+function Invoke-Starship-PreCommand {
+  $host.ui.RawUI.WindowTitle = "$env:USERNAME@$env:COMPUTERNAME`: $pwd `a"
+}
+
+Invoke-Expression (&starship init powershell)
+
+Enable-TransientPrompt
+
